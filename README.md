2935128a4f68d6724e00fe374823c08767952bff
Vandanarani <keerthanakottapalli2@gmail.com>
2025-09-25 17:00:27 +0530

    React before JSX

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React Journey🚀🚀
+
+# parcel
+- Dev builds
+- local server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - to support older browsers
+- Diagnostic - Shows beautiful errors
+- Error Handling
+- HTTPS
+- Tree Shaking - removes unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//React.createElement creates a JavaScript object (React element) which ReactDOM later converts into actual HTML elements in the browser DOM.
+//React.createElement(tag, props, children)
 // const heading = React.createElement('h1', {id: "heading"}, "Hello World");
 // console.log(heading);
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
 
-const parent = React.createElement('div', {id: "parent"}, React.createElement('div', {id: "child"}, React.createElement('h1', {className: "child-heading"}, "this is h1 tag")));
+const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, React.createElement('h1', { className: "child-heading" }, "this is React App")));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
 console.log(parent);
